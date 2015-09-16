6abbc10b6b57db90656125d83d01dd6bb51209c9
JesusRodrigoCastorHuerta <jesusrodrigocastorhuerta@gmail.com>
2015-09-16 18:29:46 -0500

    primera practica termindada y subida

---
+++ PU1.1[JESUSRODRIGOCASTORHUERTA][CALCULADORA ARITMETICA]/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string op;
+           do
+           {
+            Console.WriteLine("Proporciona el primer valor");
+            
+            int inum1 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Ingresa el segundo valor");
+            
+            int inum2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Que tipo de operacion quieres 1 = + ,2 = - , 3 = * , 4 = /");
+
+            string operacion = Console.ReadLine();
+
+            Sumar su = new Sumar(inum1, inum2);
+
+            switch (operacion)
+            
+            {
+            
+                case "1":
+            
+                    Console.WriteLine("El resultado de la suma es {0}", su.suma());
+               
+                break;
+            
+                
+                case "2":
+
+                     Console.WriteLine("El resultado de la resta es {0}", su.restar());
+                
+                break;
+                
+                case "3":
+
+                    Console.WriteLine("El resultado de la multiplicacion es {0}", su.multiplicar());
+                
+                break;
+                
+                case "4":
+                
+                    Console.WriteLine("El resultado de la divicion es {0}", su.dividir());
+
+                break;
+                
+                default:
+
+                    Console.WriteLine("Operacion no valida");
+
+                 break;
+            }
+               Console.WriteLine("Quieres hacer otra operación presione s = si y cualquier otra tecla para no");
+               
+               op = Console.ReadLine();
+
+               op = (op == "s")? op:op = "n";
+
+
+           }while(op=="s");
+            
+            
+            
+
+          
+            
+        }
+    }
+}
---
+++ PU1.1[JESUSRODRIGOCASTORHUERTA][CALCULADORA ARITMETICA]/ConsoleApplication1/ConsoleApplication1/Sumar.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Sumar
+    {
+        private int inum1, inum2;
+
+            public Sumar(int inum1,int inum2) {
+            
+                this.inum1 = inum1;
+            
+                this.inum2 = inum2;
+        
+            }      
+        
+            public int suma() {
+        
+                return inum1 + inum2;
+        
+            }
+
+            public int restar()
+            {
+
+                return inum1 - inum2;
+
+            }
+
+            public int multiplicar() {
+
+                return inum1 * inum2;
+            }
+
+            public int dividir() {
+
+                return inum1 / inum2;
+            }
+
+    }
+}
